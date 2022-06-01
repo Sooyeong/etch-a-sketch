8379b375c3519b18c3e0ab7d9330ec32dc23eabb
Sooyeong <lsy1014@nate.com>
2022-06-01 15:55:55 -0700

    Finished draft1

--- es.js
+++ es.js
@@ -1,3 +1,7 @@
+// Get number of grid by user
+
+
+
 const container=document.createElement("div");
 
 
@@ -5,7 +9,6 @@ const container=document.createElement("div");
 
 
 // create multiple grid box - I can use loop for the reference//
-
 
 const gridBox=document.createElement("div");
 
@@ -22,7 +25,6 @@ document.body.style.justifyContent="center";
 container.style.display="flex";
 container.style.width="960px";
 container.style.height="960px";
-
 container.style.justifyContent="center";
 container.style.alignItems="center";
 
@@ -37,33 +39,95 @@ var boxes=[];
 
 
 // border size ==6for each grid;
-var pixelSize=(960/16)-6;
+// We'll select var later 
 
-for (let i =0 ; i<16*16; i++){
-    boxes[i]=document.createElement("div");
-    boxes[i].style.display="inline-flex";
-    boxes[i].style.backgroundColor="white";
+var n_box=prompt("Please enter number of grid between 2-64");
 
-    boxes[i].style.borderColor="black";
-    boxes[i].style.borderStyle="solid";
-    boxes[i].style.width=`${pixelSize}px`;
-    boxes[i].style.height=`${pixelSize}px`;
+n_box=Number(n_box);
 
-    
+if (typeof(n_box)=="number" && 2<=n_box && n_box<=64){
+    var pixelSize=(960/n_box)-6;
 
-    //Add it to container
-    container.appendChild(boxes[i]);
 
-    boxes[i].addEventListener('mouseover', function style(){
-        boxes[i].style.backgroundColor="yellow";
-    });
-
-    boxes[i].addEventListener('click', function style(){
+    for (let i =0 ; i<n_box*n_box; i++){
+        boxes[i]=document.createElement("div");
+        boxes[i].style.display="inline-flex";
         boxes[i].style.backgroundColor="white";
+
+        boxes[i].style.borderColor="black";
+        boxes[i].style.borderStyle="solid";
+
+        boxes[i].style.width=`${pixelSize}px`;
+        boxes[i].style.height=`${pixelSize}px`;
+
+        
+
+        //Add it to container
+        container.appendChild(boxes[i]);
+
+        boxes[i].addEventListener('mouseover', function style(){
+            boxes[i].style.backgroundColor="grey";
+        });
+
+        boxes[i].addEventListener('click', function style(){
+            boxes[i].style.backgroundColor="white";
+        });
+    }
+
+
+
+    var clear_button = document.createElement("BUTTON");
+    clear_button.textContent="Clear All";
+
+    clear_button.addEventListener('click', function clear(){
+        // forEach --> call back 
+        boxes.forEach(box=> box.style.backgroundColor="white");
     });
+
+    document.body.appendChild(clear_button);
+
+
+
+    var btn_color = document.createElement("BUTTON");
+    btn_color.textContent="Grey";
+
+    btn_color.addEventListener('click', function clear(){
+        // forEach --> call back 
+        boxes.forEach(box=> box.addEventListener('mouseover', function style(){
+            box.style.backgroundColor="grey";
+        }));
+    });
+
+    document.body.appendChild(btn_color);
+
+    var btn_color2 = document.createElement("BUTTON");
+    btn_color2.textContent="Mixed";
+
+    btn_color2.addEventListener('click', function clear(){
+        // forEach --> call back 
+        boxes.forEach(box=> box.addEventListener('mouseover', function style(){
+            var color=["gold","blue","red","green","purple","grey","pink"]
+
+            let i=Math.floor(Math.random()*6);
+            box.style.backgroundColor=color[i];
+        }));
+    });
+
+    document.body.appendChild(btn_color2);
+
+
+
+
+
+
+
+    document.body.appendChild(container);
+
+
+
+    /* Three buttons will be added- 
+        1. Clear All
+        2. Adjust Size of the grid
+    */
+
 }
-
-
-
-document.body.appendChild(container);
-
